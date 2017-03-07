d6d7f145a1321d221c7747766ea150b710c320aa
shuxinxu <phoenix.xsx@gmail.com>
2017-03-06 18:04:39 -0800

    add data folder

--- data_cleaning.py
+++ data_cleaning.py
@@ -12,9 +12,21 @@ import matplotlib.pyplot as plt
 sheet_name=['MSCI ACWI','Barclays US Agg','HFRI FOF','Barclays TIP US Index','JPM EMBI','NCREIF']
 
 for sheet in sheet_name:
-    data=pd.read_excel('factor_timing_project_data_cleaned.xlsx',sheetname=sheet)
+    data=pd.read_excel('//data//factor_timing_project_data_cleaned.xlsx',sheetname=sheet)
     data=data.iloc[:,0:2]
     data['Date']=pd.to_datetime(data['Date'],infer_datetime_format=True)
     fig=plt.figure()
     plt.plot(data['Date'],data['Last_Price'])
     plt.legend([sheet])
+    
+    
+    
+sheet_name=['MSCI ACWI','Barclays US Agg','HFRI FOF','Barclays TIP US Index','JPM EMBI','NCREIF']
+
+for sheet in sheet_name:
+    data=pd.read_excel('/data/factor_timing_project_data_cleaned.xlsx',sheetname=sheet)
+    data=data.iloc[:,0:2]
+    data['Date']=pd.to_datetime(data['Date'],infer_datetime_format=True)
+    fig=plt.figure()
+    plt.plot(data['Date'],data['Last_Price'])
+    plt.legend([sheet])    
